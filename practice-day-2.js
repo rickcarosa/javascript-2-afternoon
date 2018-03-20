83fb65a61a7048a5ad8089f0db5cc6311750f582
Ricardo Carosa <ricardocarosa@Ricardos-MacBook-Pro.local>
2018-03-20 14:49:12 -0600

    initial commit

--- practice-day-2.js
+++ practice-day-2.js
@@ -14,7 +14,13 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+    name: "Ricky",
+    age: 25
+}
+
+
+alert(me.name);
 
 
 
@@ -25,7 +31,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+ var favoriteThings = {
+    band: "Eminem",
+    food: "Pizza",
+    person: "Me",
+    book: "been a while",
+    movie: "batman trilogy",
+    holiday: "July 4th"
+ }
 
 
 
@@ -33,7 +46,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Audi";
+favoriteThings.brand = "Nike";
 
 
 
@@ -41,7 +55,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 
 
@@ -55,15 +70,18 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {};
+var item = "firstPocket";
 
+backPack["firstPocet"]["chapstick"];
+backPack.color.black
 
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+alert(backPack);
 
 
 
@@ -73,7 +91,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
 
 
 
@@ -108,7 +126,7 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
+var methodCollection = {};
 
 
 
@@ -117,7 +135,7 @@ var user2 = {
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
 */
 
-//Code Here
+//code here
 
 
 
